84f656bccbb1040c6173c5a69d0250caba298d71
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2024-07-06 19:54:47 -0300

    feat: gerencia middleware e controller para autenticar usuário na rota de login

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/user.controller';
-import { validateExistingCredentials, validationRegistrationCredentials } from '../middlewares/auth.middleware';
+import { validateExistingCredentials, validationRegistrationCredentials, validationAuthenticateCredentials } from '../middlewares/auth.middleware';
 
 const userRouter = Router();
 const userController = new UserController();
 
 userRouter.post('/register', validationRegistrationCredentials, validateExistingCredentials, userController.createUser);
+userRouter.post('/login', validationAuthenticateCredentials, userController.authenticateUser);
 
 export default userRouter;
