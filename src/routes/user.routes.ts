ffad7c148c1428d87b650f0d57b21d5396dd5708
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2024-07-06 21:10:46 -0300

    feat: gerencia a rota de busca de informação do usuario.

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,26 @@
-import { Router } from 'express';
-import { UserController } from '../controllers/user.controller';
-import { validateExistingCredentials, validationRegistrationCredentials, validationAuthenticateCredentials } from '../middlewares/auth.middleware';
+import { Router } from "express";
+import { UserController } from "../controllers/user.controller";
+import {
+  validateExistingCredentials,
+  validationRegistrationCredentials,
+  validationAuthenticateCredentials,
+  validateIdAndToken,
+} from "../middlewares/auth.middleware";
 
 const userRouter = Router();
 const userController = new UserController();
 
-userRouter.post('/register', validationRegistrationCredentials, validateExistingCredentials, userController.createUser);
-userRouter.post('/login', validationAuthenticateCredentials, userController.authenticateUser);
+userRouter.post(
+  "/register",
+  validationRegistrationCredentials,
+  validateExistingCredentials,
+  userController.createUser
+);
+userRouter.post(
+  "/login",
+  validationAuthenticateCredentials,
+  userController.authenticateUser
+);
+userRouter.get("/:id", validateIdAndToken, userController.findUserById);
 
 export default userRouter;
